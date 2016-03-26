e73fb53660d00978284623e01126ecd51ff00e27
Jan Strube <ich@jan-strube.de>
2016-03-26 18:15:03 +0100

    Add twitter4j properties. May now tweet!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TwitterBot1910
 A twitter bot that tweets at 19:10
+
+see http://staticvoidgames.com/tutorials/howTo/twitterBot
--- src/main/java/net/struwi/twitterbot1910/Main.java
+++ src/main/java/net/struwi/twitterbot1910/Main.java
@@ -1,12 +1,15 @@
 package net.struwi.twitterbot1910;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
+        Status status = twitter.updateStatus("hello world");
     }
 
 }
