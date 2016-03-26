3c10c365a7d2ba71ee180138a9b4b011f4793da9
Jan Strube <ich@jan-strube.de>
2016-03-26 17:47:10 +0100

    Add main class and pom file

---
+++ src/main/java/net/struwi/twitterbot1910/Main.java
@@ -0,0 +1,12 @@
+package net.struwi.twitterbot1910;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Twitter twitter = TwitterFactory.getSingleton();
+    }
+
+}
