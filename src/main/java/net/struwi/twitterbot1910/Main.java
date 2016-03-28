7ccc1558b8939150483d1bd2b6ca530fc7235416
Jan Strube <ich@jan-strube.de>
2016-03-28 13:28:04 +0200

    Tweet time at 19:10

--- src/main/java/net/struwi/twitterbot1910/Main.java
+++ src/main/java/net/struwi/twitterbot1910/Main.java
@@ -5,11 +5,34 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
     public static void main(String[] args) throws TwitterException {
-        Twitter twitter = TwitterFactory.getSingleton();
-        Status status = twitter.updateStatus("hello world");
+        System.out.println("Starting bot");
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TweetTask(), 0L, 60000L);
+    }
+
+    private static class TweetTask extends TimerTask {
+
+        public void run() {
+            LocalTime now = LocalTime.now();
+            if (now.getHour() == 19 && now.getMinute() == 10) {
+                System.out.println("Tweeting time");
+                Twitter twitter = TwitterFactory.getSingleton();
+                try {
+                    Status status = twitter.updateStatus("It´s 19:10");
+                }
+                catch (TwitterException ex) {
+                    System.out.println("Error tweeting: " + ex.getMessage());
+                }
+            }
+        }
+
     }
 
 }
